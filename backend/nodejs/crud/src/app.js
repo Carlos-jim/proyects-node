4f059ec8931a750440df89c05fb07e4482264ac6
Carlos Jimenez <carlosmjimenezg@gmail.com>
2024-05-16 13:03:54 -0400

    ..

---
+++ backend/nodejs/crud/src/app.js
@@ -0,0 +1,16 @@
+import express from 'express'
+import empleadosRoutas from './router/empleados.routes.js'
+import indexRoutas from './router/index.routes.js'
+const app = express()
+
+app.use(express.json())
+
+app.use('/api', empleadosRoutas)
+app.use(indexRoutas)
+
+app.use((req, res, next) => {
+    res.status(404).json({ message: "Not found" });
+  });
+  
+
+  export default app
--- backend/nodejs/crud/src/index.js
+++ backend/nodejs/crud/src/index.js
@@ -1,18 +1,5 @@
-import express from 'express'
-import empleadosRoutas from './router/empleados.routes.js'
-import indexRoutas from './router/index.routes.js'
+import app from "./app.js";
+import { PORT } from "./config.js";
 
-const app = express()
-const port = 3000
-app.use(express.json())
-
-app.use('/api', empleadosRoutas)
-app.use(indexRoutas)
-
-app.use((req, res, next) => {
-    res.status(404).json({ message: "Not found" });
-  });
-  
-app.listen(port)
-
-console.log("Servidor corriendo en el puerto", port)
+app.listen(PORT);
+console.log(`Server on port http://localhost:${PORT}`);
