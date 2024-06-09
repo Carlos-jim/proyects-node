bb82ecf34bc0e672272348b0ebd2bd2f64fee53d
Carlos Jimenez <carlosmjimenezg@gmail.com>
2024-06-08 20:36:00 -0400

    ....

--- backend/nodejs/PUNTO-VENTA/src/router/productos.routes.js
+++ backend/nodejs/PUNTO-VENTA/src/router/productos.routes.js
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 import { check } from 'express-validator';
-import {  crearProducto } from '../controllers/productos.controller.js';
+import {  crearProducto,removeProducto, getProductos, getUnProducto, editProducto } from '../controllers/productos.controller.js';
 
 const router = Router()
 
@@ -9,6 +9,14 @@ router.post('/crear-producto', [
     check('precio', 'El precio del producto es obligatorio y debe ser un número').isFloat({ min: 0 }),
     check('stock', 'El stock del producto es obligatorio y debe ser un número entero').isInt({ min: 0 })
 ], crearProducto)
+
+router.get('/obtener-productos', getProductos)
+router.get('/obtener-producto/:id', getUnProducto)
+
+router.put('/editar-producto/:id', editProducto)
+router.delete('eliminar-producto/:id', removeProducto)
+
+
 
 
 export default router
